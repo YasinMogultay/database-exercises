e65fe72dba93cc8fdb02cd569dc68343fca2e296
Yasin Mogultay <yasin.mglty@gmail.com>
2021-04-01 10:30:35 -0500

    completed limit exercise

---
+++ limit_exercises.sql
@@ -0,0 +1,7 @@
+use employees;
+
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5;
+
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5 OFFSET 45;
+
+select * from salaries;
---
+++ order_by_exercises.sql
@@ -0,0 +1,11 @@
+use employees;
+
+# Part One
+SELECT * FROM employees WHERE first_name IN ('Irena','Vidya', 'Maya') ORDER BY first_name AND last_name LIKE 'Reutenauer';
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name;
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY  last_name, first_name;
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no;
+
