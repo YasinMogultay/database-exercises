3b4229804c9c7c2a9ba17e04ecbc1c48ddf03a6e
Yasin Mogultay <yasin.mglty@gmail.com>
2021-04-08 15:46:33 +0300

    adjusted

--- database_design_lecture.sql
+++ database_design_lecture.sql
@@ -7,30 +7,36 @@ DROP TABLE IF EXISTS quotes;
 DROP TABLE IF EXISTS topics;
 DROP TABLE IF EXISTS authors;
 
-CREATE TABLE quotes (
-id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-author_first_name VARCHAR(50) DEFAULT 'NONE',
-author_last_name  VARCHAR(100) NOT NULL,
-content TEXT NOT NULL,
-PRIMARY KEY (id)
+CREATE TABLE quotes
+(
+    id                INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    author_first_name VARCHAR(50) DEFAULT 'NONE',
+    author_last_name  VARCHAR(100) NOT NULL,
+    content           TEXT         NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE authors (
- id INT UNSIGNED NOT NULL AUTO_INCREMENT,
- first_name VARCHAR(50),
- last_name  VARCHAR(100) NOT NULL,
- PRIMARY KEY (id)
+CREATE TABLE authors
+(
+    id         INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name  VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-ALTER TABLE quotes DROP COLUMN author_first_name;
-ALTER TABLE quotes DROP COLUMN author_last_name;
-ALTER TABLE quotes ADD COLUMN author_id INT UNSIGNED NOT NULL;
-ALTER TABLE quotes ADD FOREIGN KEY (author_id) REFERENCES authors(id) ON DELETE NO ACTION ON UPDATE CASCADE ;
+ALTER TABLE quotes
+    DROP COLUMN author_first_name;
+ALTER TABLE quotes
+    DROP COLUMN author_last_name;
+ALTER TABLE quotes
+    ADD COLUMN author_id INT UNSIGNED NOT NULL;
+ALTER TABLE quotes
+    ADD FOREIGN KEY (author_id) REFERENCES authors (id) ON DELETE NO ACTION ON UPDATE CASCADE;
 
-INSERT INTO authors(first_name, last_name) VALUES
-('Yasin', 'Mogultay'),
-('Carlos', 'Padilla'),
-('William', 'Baldridge');
+INSERT INTO authors(first_name, last_name)
+VALUES ('Yasin', 'Mogultay'),
+       ('Carlos', 'Padilla'),
+       ('William', 'Baldridge');
 
 INSERT INTO quotes(author_id, content)
 VALUES ((select id from authors where first_name = 'Yasin' and last_name = 'Mogultay'),
@@ -50,52 +56,69 @@ VALUES ((select id from authors where first_name = 'Yasin' and last_name = 'Mogu
 
 SELECT a.last_name, a.first_name, content
 FROM authors as a
-JOIN quotes q on a.id = q.author_id
+         JOIN quotes q on a.id = q.author_id
 WHERE last_name = 'Mogultay';
 
 
-CREATE TABLE topics (
-id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-name VARCHAR(255),
-PRIMARY KEY (id)
+CREATE TABLE topics
+(
+    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
 
-SELECT id FROM quotes;
-SELECT id FROM authors;
-SELECT * FROM topics;
-SELECT * FROM quote_topic;
+SELECT id
+FROM quotes;
+SELECT id
+FROM authors;
+SELECT *
+FROM topics;
+SELECT *
+FROM quote_topic;
 
 
-INSERT INTO topics(name) VALUES
-('Coding'),
-('Life'),
-('Cooking'),
-('Sports'),
-('Travel');
+INSERT INTO topics(name)
+VALUES ('Coding'),
+       ('Life'),
+       ('Cooking'),
+       ('Sports'),
+       ('Travel');
 
-CREATE TABLE quote_topic (
-quote_id INTEGER UNSIGNED NOT NULL,
-topic_id INTEGER UNSIGNED NOT NULL,
-FOREIGN KEY (quote_id) REFERENCES quotes(id),
-FOREIGN KEY (topic_id) REFERENCES topics(id)
+CREATE TABLE quote_topic
+(
+    quote_id INTEGER UNSIGNED NOT NULL,
+    topic_id INTEGER UNSIGNED NOT NULL,
+    FOREIGN KEY (quote_id) REFERENCES quotes (id),
+    FOREIGN KEY (topic_id) REFERENCES topics (id)
 );
 
 
 
-INSERT INTO quote_topic (quote_id, topic_id) VALUES
-(1,1), (2,2), (3,1), (4,2), (5,5),
-(2,1), (2,3), (3,4), (4,1), (4,3), (5,2);
+INSERT INTO quote_topic (quote_id, topic_id)
+VALUES (1, 1),
+       (2, 2),
+       (3, 1),
+       (4, 2),
+       (5, 5),
+       (2, 1),
+       (2, 3),
+       (3, 4),
+       (4, 1),
+       (4, 3),
+       (5, 2);
 
 
-SELECT q.content as Quote, CONCAT(a.first_name, ' ', a.last_name) AS Full_Name,t.name
-as Topic
+SELECT q.content                              as Quote,
+       CONCAT(a.first_name, ' ', a.last_name) AS Full_Name,
+       t.name
+                                              as Topic
 FROM quotes q
-INNER JOIN quote_topic qt ON q.id = qt.quote_id
-INNER JOIN topics t ON qt.topic_id = t.id
-INNER JOIN authors a ON q.author_id = a.id
+         INNER JOIN quote_topic qt ON q.id = qt.quote_id
+         INNER JOIN topics t ON qt.topic_id = t.id
+         INNER JOIN authors a ON q.author_id = a.id
 WHERE qt.topic_id IN
-    (
-        SELECT t.id
-        FROM topics t
-        WHERE t.name = 'Coding'
-        );
+      (
+          SELECT t.id
+          FROM topics t
+          WHERE t.name = 'Coding'
+      );
