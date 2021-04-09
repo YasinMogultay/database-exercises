b6e8e5c9348e25d0ed1784b8c9183e03e109018e
Yasin Mogultay <yasin.mglty@gmail.com>
2021-04-09 17:57:17 +0300

    adjusted

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,13 +2,14 @@ USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 
-CREATE TABLE albums (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(100),
-    name VARCHAR (100),
+CREATE TABLE albums
+(
+    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist       VARCHAR(100),
+    name         VARCHAR(100),
     release_date INT,
-    genre VARCHAR (200),
-    sales FLOAT(6,2),
+    genre        VARCHAR(200),
+    sales        FLOAT(6, 2),
     PRIMARY KEY (id)
 );
 -- AUTO_INCREMENT = 1000 we can use this to set id start from 1000; after last parentheses before ;
