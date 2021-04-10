9f05eaf1371e4e9f1625ed2521a53ba243809811
Yasin Mogultay <yasin.mglty@gmail.com>
2021-04-10 14:25:28 +0300

    adjusted

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,8 +1,14 @@
 USE codeup_test_db;
 
-DELETE FROM albums WHERE release_date > 1991;
+DELETE
+FROM albums
+WHERE release_date > 1991;
 
-DELETE FROM albums WHERE genre LIKE 'disco%';
+DELETE
+FROM albums
+WHERE genre LIKE 'disco%';
 
-DELETE FROM albums WHERE artist LIKE '%Whitney%';
+DELETE
+FROM albums
+WHERE artist LIKE '%Whitney%';
 
