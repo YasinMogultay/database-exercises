ff0f548b7cc0d85dd3278858315fda757621ffc8
Yasin Mogultay <yasin.mglty@gmail.com>
2021-04-20 23:22:32 +0300

    adjust

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -12,6 +12,7 @@ SELECT * FROM employees WHERE birth_date AND MONTH(birth_date) = 12 AND DAY(birt
 SELECT * FROM employees WHERE MONTH(birth_date) = 12 AND DAY(birth_date) = 25 AND (DAY(hire_date) BETWEEN 1990 AND 1999);
 
 # Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
+
 SELECT * FROM employees WHERE MONTH(birth_date) = 12 AND DAY(birth_date) = 25 AND (YEAR(hire_date) BETWEEN 1990 AND 1999) ORDER BY birth_date, hire_date DESC;
 
 SELECT *, DATEDIFF(NOW(), hire_date) FROM employees WHERE MONTH(birth_date) = 12 AND DAY(birth_date) = 25 AND (YEAR(hire_date) BETWEEN 1990 AND 1999) ORDER BY birth_date, hire_date DESC;
